c853b57ecff6c6918f114a78306a83d6cb196e17
Vladislav <funte@>
2023-08-23 19:21:40 +0300

    complite

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,20 +1,35 @@
+import os
 import csv
 
-from django.conf import settings
+from foodgram import settings
 from django.core.management import BaseCommand
-
 from recipes.models import Ingredient
+
+from progress.bar import IncrementalBar
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        data_path = settings.BASE_DIR
-        with open(
-            f'{data_path}/data/ingredients.csv',
-            'r',
-            encoding='utf-8'
-        ) as file:
-            reader = csv.DictReader(file)
-            Ingredient.objects.bulk_create(
-                Ingredient(**data) for data in reader)
-        self.stdout.write(self.style.SUCCESS('Ингридиенты успешно загружены!'))
+    help = "Загрузка ингредиентов из data/ingredients.csv"
+
+    def create_ingredients(self, row):
+        name, measurement = row
+        Ingredient.objects.get_or_create(name=name,
+                                         measurement=measurement)
+
+    def load_ingredients(self, *args, **options):
+        path = os.path.join(settings.BASE_DIR, 'ingredients.csv')
+        row_count = 0
+        with open(path, 'r', encoding='utf-8') as file:
+            for _ in file:
+                row_count += 1
+        with open(path, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            bar = IncrementalBar('Загрузка...'.ljust(30), max=row_count)
+            next(reader)
+            for row in reader:
+                self.create_ingredients(row)
+                bar.next()
+            bar.finish()
+        self.stdout.write(
+            self.style.SUCCESS("Все ингредиенты успешно загружены!")
+        )
